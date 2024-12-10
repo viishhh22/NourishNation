4fff0efe8c3d4dbf22b8ac63fe293d460dcf0b0b
Vishakha Patil <127527714+viishhh22@users.noreply.github.com>
2024-12-10 23:48:16 +0530

    Add files via upload

---
+++ PM-Poshan-main/static/scripts/home/home.js
@@ -0,0 +1,31 @@
+// hard coded passwords for login page
+// change it to preferred DB 
+
+document.getElementById("loginbtn").onclick = function() {
+    console.log(document.getElementById("dashselect").value)
+
+    if(document.getElementById("dashselect").value == 0 && document.getElementById("email").value == "school@gmail.com" && document.getElementById("password").value == "pass"){
+        window.location.href = "school_dash"
+    }
+    
+    else if(document.getElementById("dashselect").value == 1 && document.getElementById("email").value == "admin@gmail.com" && document.getElementById("password").value == "pass"){
+        window.location.href = "performance"
+    }
+
+    //student has admission number and dob as password for simplicity 
+    else if(document.getElementById("dashselect").value == 2 && document.getElementById("email").value == "1234" && document.getElementById("password").value == "03092002"){
+        window.location.href = "student_dash"
+    }
+
+    else if(document.getElementById("dashselect").value == 3 && document.getElementById("email").value == "natadmin@gmail.com" && document.getElementById("password").value == "pass"){
+        window.location.href = "performance/detailed"
+    }
+
+    else{
+        alert("\nInvalid Login Details")
+    }
+}
+
+document.getElementById("forgotpwdbtn").onclick = function() {
+    alert("\nPlease contact officer@pmposhan.com \nfor account related queries!")
+}
